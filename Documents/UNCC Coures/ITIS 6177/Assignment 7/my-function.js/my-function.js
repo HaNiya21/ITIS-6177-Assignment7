211050c6173197372ac0d60d85866b7bf781dbd9
Hana Geremew <39864956+HaNiya21@users.noreply.github.com>
2025-03-06 19:24:54 -0500

    Delete Documents/UNCC Coures/ITIS 6177/Assignment 7/my-function.js directory

--- Documents/UNCC Coures/ITIS 6177/Assignment 7/my-function.js/my-function.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async (event) => {
-    const keyword = event.queryStringParameters.keyword || '';
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify(`Hana says ${keyword}`),
-    };
-    return response;
-};
