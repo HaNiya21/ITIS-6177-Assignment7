3f62bb5a1d339cd2ecdf38eaac4e355870c56970
Hana Geremew <hanaberehanu@gmail.com>
2025-03-07 09:21:44 -0500

    updated the my-function.js

--- my-function.js
+++ my-function.js
@@ -2,6 +2,11 @@ exports.handler = async (event) => {
     const keyword = event.queryStringParameters.keyword || '';
     const response = {
         statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*', // Allow all origins
+            'Access-Control-Allow-Methods': 'GET', // Allow GET requests
+            'Access-Control-Allow-Headers': 'Content-Type', // Allow specific headers
+        },
         body: JSON.stringify(`Hana says ${keyword}`),
     };
     return response;
